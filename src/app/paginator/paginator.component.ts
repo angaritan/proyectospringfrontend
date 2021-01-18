2ed9588fd01613c6fa06619323dade9bd62b6e61
angaritan <chamanemp@yahoo.es>
2021-01-18 02:13:42 -0500

    faeture2

--- src/app/paginator/paginator.component.ts
+++ src/app/paginator/paginator.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class PaginatorComponent implements OnInit {
  @Input() paginador: any;
  paginas : number[];
+ feature2: any;
   constructor() { }
 
   ngOnInit(): void {
